4fd5b224c9155dc4cbe29d440ba982dea67e7e70
ELFAKHAR <mohammed.elfakhar@ztechengineerin.com>
2021-07-13 11:41:51 +0100

    facebook_authentication

---
+++ android/app/src/main/kotlin/com/example/odhia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.odhia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:odhia/Home.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
